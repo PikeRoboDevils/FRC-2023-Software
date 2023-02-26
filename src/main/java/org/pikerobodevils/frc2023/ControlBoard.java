1497271ddb2b5f82b77b1fa16075c405eac24fec
Ryan Blue <ryanzblue@gmail.com>
2023-02-25 23:20:58 -0500

    Remove extra motor on drivetrain

--- src/main/java/org/pikerobodevils/frc2023/ControlBoard.java
+++ src/main/java/org/pikerobodevils/frc2023/ControlBoard.java
@@ -5,24 +5,21 @@
 
 package org.pikerobodevils.frc2023;
 
-import static org.pikerobodevils.frc2023.Constants.ControlBoardConstants.*;
-
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 public class ControlBoard {
-  private final CommandJoystick leftStick = new CommandJoystick(LEFT_STICK);
-  private final CommandJoystick rightStick = new CommandJoystick(RIGHT_STICK);
-  private final CommandXboxController xBox = new CommandXboxController(5);
+  public final CommandXboxController driver = new CommandXboxController(0);
+
+  public final CommandXboxController operator = new CommandXboxController(1);
 
   public double getSpeed() {
-    double leftY = -xBox.getLeftY() * .75;
+    double leftY = -driver.getLeftY();
     return Math.signum(leftY) * Math.pow(MathUtil.applyDeadband(leftY, .04), 2);
   }
 
   public double getTurn() {
-    double rightX = -xBox.getRightX() * .75;
+    double rightX = -driver.getRightX();
     return Math.signum(rightX) * Math.pow(MathUtil.applyDeadband(rightX, .04), 2);
   }
 }
