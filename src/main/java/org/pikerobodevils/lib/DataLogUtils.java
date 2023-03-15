1d2333c8ec381b2ef34bbf68a5931ee68a3c167b
Ryan Blue <ryanzblue@gmail.com>
2023-03-15 03:23:13 -0400

    add warn function to DataLogUtils

--- src/main/java/org/pikerobodevils/lib/DataLogUtils.java
+++ src/main/java/org/pikerobodevils/lib/DataLogUtils.java
@@ -8,12 +8,20 @@ package org.pikerobodevils.lib;
 import static edu.wpi.first.wpilibj.DataLogManager.log;
 
 import com.revrobotics.CANSparkMax;
+import edu.wpi.first.hal.DriverStationJNI;
 import edu.wpi.first.util.datalog.StringLogEntry;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.util.WPILibVersion;
 
 public class DataLogUtils {
+  private static StringLogEntry m_messageLog;
   public static final String METADATA_PATH = "/RealMetadata/";
+
+  private static void init() {
+    if (m_messageLog == null) {
+      m_messageLog = new StringLogEntry(DataLogManager.getLog(), "messages");
+    }
+  }
 
   public static void logMetadataEntry(String key, Object value) {
     var entry = new StringLogEntry(DataLogManager.getLog(), METADATA_PATH + key);
@@ -38,5 +46,15 @@ public class DataLogUtils {
         System.getProperty("java.vendor") + " " + System.getProperty("java.version"));
     logMetadataEntry("WPILib Version", WPILibVersion.Version);
     logMetadataEntry("RevLib Version", CANSparkMax.kAPIVersion);
+  }
+
+  public static void logNoPrint(String message) {
+    init();
+    m_messageLog.append(message);
+  }
+
+  public static void warning(String warning) {
+    DriverStationJNI.sendError(true, 1, false, "WARNING: " + warning, "", "", true);
+    logNoPrint("WARNING: " + warning);
   }
 }
